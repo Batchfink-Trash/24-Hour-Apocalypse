0cf215deaf902df356a0e9e00391c73fd191c6b1
thegamingboffin <thegamingboffin@gmail.com>
2015-02-08 11:31:12 +0000

    -Added more weapons
    Added shovel, cane, handgun, box, rake, garden fork and broom

--- 24 Hour Apocolypse/Weapons.py
+++ 24 Hour Apocolypse/Weapons.py
@@ -2,25 +2,57 @@ import random
 
 """ This module contains classes for all of the weapons useable in the game"""
 
+""" List of all attack types for future reference:
+    - Smack
+    - Stab
+    - Push
+    - Shoot
+    
+"""
+    
+class Rake(object):
+    def smack():
+        pass
+
+class Broom(object):
+    def smack():
+        pass
+    
+class GardenFork(object):
+    def stab():
+        pass
+    
+class Shovel(object):
+    def smack():
+        pass
+    
+class Box(object):
+    def push():
+        pass
+
+class Handgun(object):
+    ammo = 0
+    def shoot():
+        accuracy = random.randint(0, 3)
+        return 3 * accuracy
+
+class Cane(object):
+    def smack():
+        pass
+
 class Crowbar(object):
     def stab():
         pass
-    def swipe():
-        pass
 
-class bat(object):
+class Bat(object):
     def smack():
         pass
-    def jab():
-        pass
 
-class sword(object):
-    def swipe():
-        pass
+class Knife(object):
     def stab():
         pass
 
-class crossbow(object):
+class Crossbow(object):
     ammo = 0
     def shoot():
         accuracy = random.randint(0, 5)
